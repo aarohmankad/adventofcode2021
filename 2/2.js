3b6e500b866aece54c6af79e8da95e59de8d5720
Aaroh Mankad <amankad@plaid.com>
2021-12-01 21:38:00 -0800

    Day 2 redo

--- 2/2.js
+++ 2/2.js
@@ -1,9 +1,32 @@
 const fs = require("fs");
 
+const move = (directions) => {
+  let horizontal = 0;
+  let depth = 0;
+  let aim = 0;
+  for (let i = 0; i < directions.length; i++) {
+    let [direction, distance] = directions[i].split(" ");
+    distance = parseInt(distance);
+    switch (direction) {
+      case "forward":
+        horizontal += distance;
+        depth += aim * distance;
+        break;
+      case "up":
+        aim -= distance;
+        break;
+      case "down":
+        aim += distance;
+        break;
+    }
+  }
+  return horizontal * depth;
+};
+
 let buffer = fs.readFileSync(__dirname + "/input");
-const numbers = buffer
+const directions = buffer
   .toString()
   .split("\n")
   .filter((n) => n.length);
 
-console.log();
+console.log(move(directions));
